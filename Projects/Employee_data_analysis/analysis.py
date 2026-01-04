ab5df82f76ddd23e4377e1ffb8b0255fa5d9b67e
Kushagra Saini <kushagra.saini19@gmail.com>
2026-01-04 17:58:42 +0530

    Completed project

--- Projects/Employee_data_analysis/analysis.py
+++ Projects/Employee_data_analysis/analysis.py
@@ -15,8 +15,12 @@ summary = {
 }
 print(summary)
 
+# Minimum , maximum , mean salary of each department -->
+
 dept_salary = df.groupby("Team")['Salary'].agg(["mean", "min", "max"])
 print(dept_salary)
+
+# Bar graph showing average salary by department --> 
 
 plt.figure(figsize=(8,6))
 dept_salary['mean'].plot(kind="bar")
@@ -27,6 +31,28 @@ plt.tight_layout()
 plt.savefig("Average_salary.png",dpi = 300, bbox_inches = "tight")
 plt.show()
 
-gender_salary = df.groupby('Gender')['Salary'].agg(["mean", "max", "min"])
-print(gender_salary)
+# Gender pay gap -->
 
+gender_salary = df.groupby('Gender')['Salary'].mean()
+pay_gap = gender_salary.max() - gender_salary.min()
+print("Gender Pay Gap :",pay_gap) 
+
+# Salary Outlier detection --> 
+
+Q1 = np.percentile(df["Salary"],25)
+Q2 = np.percentile(df["Salary"],75)
+IQR = Q2 - Q1
+
+outliers = df[
+    (df["Salary"] < Q1 - 1.5 * IQR) | (df["Salary"] > Q2 + 1.5 * IQR)
+]
+print("Outliers :\n", outliers)
+
+# Salary box plot --> 
+
+plt.boxplot(df["Salary"])
+plt.title("Salary distrubution & outliers")
+plt.ylabel("Salary -->")
+plt.tight_layout()
+plt.savefig("Salary_distribution.png", dpi = 300, bbox_inches = "tight")
+plt.show()
